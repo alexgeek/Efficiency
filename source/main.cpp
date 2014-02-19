bab465ee857ae99d15f1722d6e5add8af21bac37
Alexander Brook Perry <alex@alexgeek.co.uk>
2014-02-19 15:34:52 +0000

    Makefile can now compile and link c sources too

--- source/main.cpp
+++ source/main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <glm/gtc/type_ptr.hpp>
 #include <glm/gtx/string_cast.hpp>
+#include "util.h"
 
 
 void glfw_error_callback(int error, const char* description)
@@ -172,50 +173,6 @@ int run(lua_State *l, std::string file)
 }
 
 
-// http://r3dux.org/2012/07/a-simple-glfw-fps-counter/
-double calcFPS()
-{
-	// Static values which only get initialised the first time the function runs
-	static double theTimeInterval = 1.0;
-	static double t0Value       = glfwGetTime(); // Set the initial time to now
-	static int    fpsFrameCount = 0;             // Set the initial FPS frame count to 0
-	static double fps           = 0.0;           // Set the initial FPS value to 0.0
- 
-	// Get the current time in seconds since the program started (non-static, so executed every time)
-	double currentTime = glfwGetTime();
- 
-	// Ensure the time interval between FPS checks is sane (low cap = 0.1s, high-cap = 10.0s)
-	// Negative numbers are invalid, 10 fps checks per second at most, 1 every 10 secs at least.
-	if (theTimeInterval < 0.1)
-	{
-		theTimeInterval = 0.1;
-	}
-	if (theTimeInterval > 10.0)
-	{
-		theTimeInterval = 10.0;
-	}
- 
-	// Calculate and display the FPS every specified time interval
-	if ((currentTime - t0Value) > theTimeInterval)
-	{
-		// Calculate the FPS as the number of frames divided by the interval in seconds
-		fps = (double)fpsFrameCount / (currentTime - t0Value);
-		
-		std::cout << "FPS " << fps << std::endl;
-
-		// Reset the FPS frame counter and set the initial time to be now
-		fpsFrameCount = 0;
-		t0Value = glfwGetTime();
-	}
-	else // FPS calculation time interval hasn't elapsed yet? Simply increment the FPS frame counter
-	{
-		fpsFrameCount++;
-	}
- 
-	// Return the current FPS - doesn't have to be used if you don't want it!
-	return fps;
-}
-
 int main(void)
 {
     GLFWwindow* window;
@@ -344,7 +301,7 @@ int main(void)
         run(lua_state, "render.lua");
         
         if(glfwGetKey(window, 'F'))
-            calcFPS();
+            std::cout << "FPS: " << calcFPS(glfwGetTime()) << std::endl;
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
---
+++ source/util.h
@@ -0,0 +1,9 @@
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+double calcFPS(double time);
+
+#ifdef __cplusplus
+}
+#endif
